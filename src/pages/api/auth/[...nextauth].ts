f15a7c614b87a3228c68bd245e922f6d54a2a8ff
Gustavo <kyoro_souza@hotmail.com>
2022-01-05 17:24:34 -0300

    ✨ github login implementation

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,17 @@
+import NextAuth from 'next-auth';
+import GitHubProvider from "next-auth/providers/github";
+
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      
+    })
+  ],
+  secret: process.env.SECRET,
+  session: {
+    strategy:'jwt'
+  },
+})
+
