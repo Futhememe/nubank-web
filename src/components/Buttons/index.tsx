ee4b2c1e11df1b2e2415d89ca0d6cd9797c400be
Gustavo <kyoro_souza@hotmail.com>
2022-01-05 17:18:58 -0300

    🎨 new props for Sign in button

--- src/components/Buttons/index.tsx
+++ src/components/Buttons/index.tsx
@@ -1,12 +1,13 @@
-import { motion } from 'framer-motion';
+import { HTMLMotionProps, motion } from 'framer-motion';
 import { ButtonContainer } from './style';
 
-export const SignInButton = () => {
+export const SignInButton = ({...rest}: HTMLMotionProps<'button'>) => {
   return(
     <ButtonContainer 
       type='button'
       whileHover={{ y: -2 }}
       whileTap={{ y: 2 }}
+      {...rest}
     >
       <img src='/githublogo.svg' alt='github' />
       Login com Github
