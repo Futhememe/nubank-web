0de3255f4a9855b014d21077db7446e0213750e2
Gustavo <kyoro_souza@hotmail.com>
2022-01-08 00:15:27 -0300

    💄 add theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  background: '#F7F7FF',
+  primary: '#820AD1',
+  black: '#323232',
+  white: '#FFF',
+  lightGray: '#E8E8F0',
+  darkGray: '#7A7A80',
+  orange: '#FF7900',
+  green: '#00DD16',
+  blue: '#009BDD'
+}
