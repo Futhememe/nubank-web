1e326c13f9e78c450b6debc5da65016536c3ac8e
Gustavo <kyoro_souza@hotmail.com>
2022-01-20 22:51:41 -0300

    ✨ drag n drop features script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -35,6 +35,9 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600&display=swap" rel="stylesheet" />
+          <script src="https://unpkg.com/react@16.3.1/umd/react.development.js"></script>
+          <script src="https://unpkg.com/react-beautiful-dnd@x.x.x/dist/react-beautiful-dnd.js"></script>
+          <script src="https://unpkg.com/react-dom@16.3.1/umd/react-dom.development.js"></script>
         </Head>
         <body>
           <Main />
